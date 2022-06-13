f17d92157f64fc9a79473b930f082ed5770af31d
Bekzod <bekzodturgunoff@gmail.com>
2022-06-13 22:11:02 +0500

    somthing

--- index.js
+++ index.js
@@ -1,38 +1,33 @@
-// const fs = require("fs/promises")
+const fs = require("fs/promises");
 
-// async function example(){
-//     try{
-//         const content = "Hello world, this is a async way to write"
-//         await fs.writeFile("/Users/lenovo/async_node.txt", content)
-//     }catch(err){
-//         console.log(err)
-//     }
-// }
-
-// example()
-
-// const fs = require("fs")
-
-// const content = "This is a test of appending file in Node.js"
-
-// fs.appendFile("file.log", content, err =>{
-//     if(err){
-//         console.log(err)
-//     }
-// })
-
-
-
-const fs = require("fs/promises")
-
-async function example(){
-    try{
-        const content = "This is a test promises"
-        await fs.appendFile("/Users/lenovo/test_promises.txt", content)
-        
-    }catch (err){
-        console.log(err)
-    }
+async function example() {
+  try {
+    const content = "Hello world, this is a async way to write";
+    await fs.writeFile("/Users/lenovo/async_node.txt", content);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-example( )  
+example();
+
+const fs = require("fs");
+
+const content = "This is a test of appending file in Node.js";
+
+fs.appendFile("file.log", content, (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
+
+const fs = require("fs/promises");
+async function example() {
+  try {
+    const content = "This is a test promises";
+    await fs.appendFile("/Users/lenovo/test_promises.txt", content);
+  } catch (err) {
+    console.log(err);
+  }
+}
+example();
